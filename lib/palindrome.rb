009ea55ca8bd079f353bb21c621a97bcb9b1bca6
Ian <iMacD2.0@gmail.com>
2015-02-13 19:49:32 -0800

    String#palindrome method working

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,7 +1,17 @@
 require('pry')
 
 class String
-  define_method(:palindrom) do
-    #code
+  define_method(:palindrome) do
+    caps = false
+    if self.!=(self.downcase())
+      caps = true
+    end
+    word = self.downcase().reverse
+    # word = word.reverse()
+    if caps.==(true)
+      word.capitalize()
+    elsif caps.==(false)
+      word
+    end    
   end
 end
--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -2,22 +2,31 @@ require('pry')
 require('palindrome')
 
 describe('String#palindrome') do
-  it('Returns a the object word, spelled backwards') do
-    expect('word'.palindrome()).to(eq('drow'))
+  it('Returns a the object word, spelled backwards, maintaining caps') do
+    expect('Word'.palindrome()).to(eq('Drow'))
   end
-  it('Returns a the object word, spelled backwards') do
+  it('Returns a the object word, spelled backwards, maintaining caps') do
+    expect('wOrd'.palindrome()).to(eq('Drow'))
+  end
+  it('Returns a the object word, spelled backwards, maintaining caps') do
     expect('cat'.palindrome()).to(eq('tac'))
   end
-  it('Returns a the object word, spelled backwards') do
-    expect('racecar'.palindrome()).to(eq('racecar'))
+  it('Returns a the object word, spelled backwards, maintaining caps') do
+    expect('Racecar'.palindrome()).to(eq('Racecar'))
   end
-  it('Returns a the object word, spelled backwards') do
+  it('Returns a the object word, spelled backwards, maintaining caps') do
     expect('rotor'.palindrome()).to(eq('rotor'))
   end
-  it('Returns a the object word, spelled backwards') do
-    expect('sagas'.palindrome()).to(eq('sagas'))
+  it('Returns a the object word, spelled backwards, maintaining caps') do
+    expect('Sagas'.palindrome()).to(eq('Sagas'))
   end
-  it('Returns a the object word, spelled backwards') do
+  it('Returns a the object word, spelled backwards, maintaining caps') do
     expect('tattarrattat'.palindrome()).to(eq('tattarrattat'))
   end
+  # it('Returns a the object word, spelled backwards, maintaining caps') do
+  #   expect(404.palindrome()).to(eq(404))
+  # end
+  # it('Returns a the object word, spelled backwards, maintaining caps') do
+  #   expect(123.palindrome()).to(eq(321))
+  # end
 end
